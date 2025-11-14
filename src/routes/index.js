214c8b697da58b1b9d7d9bec05e9460436307083
muktanur <muktanurrahman7@gmail.com>
2025-11-15 02:55:06 +0530

    reverse

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,23 @@
-// import { authRoutes } from "./auth.js";
-// import { orderRoutes } from "./order.js";
-// import { categoryRoutes, productRoutes } from "./products.js";
-
-// const prefix = "/api";
-
-// export const registerRoutes = async (fastify) => {
-//   fastify.register(authRoutes, { prefix: prefix });
-//   fastify.register(productRoutes, { prefix: prefix });
-//   fastify.register(categoryRoutes, { prefix: prefix });
-//   fastify.register(orderRoutes, { prefix: prefix });
-// };
-
 import { authRoutes } from "./auth.js";
 import { orderRoutes } from "./order.js";
 import { categoryRoutes, productRoutes } from "./products.js";
 
+const prefix = "/api";
+
 export const registerRoutes = async (fastify) => {
-  fastify.register(authRoutes,      { prefix: "/api" });
-  fastify.register(productRoutes,   { prefix: "/api" });
-  fastify.register(categoryRoutes,  { prefix: "/api" });
-  fastify.register(orderRoutes,     { prefix: "/api" });
+  fastify.register(authRoutes, { prefix: prefix });
+  fastify.register(productRoutes, { prefix: prefix });
+  fastify.register(categoryRoutes, { prefix: prefix });
+  fastify.register(orderRoutes, { prefix: prefix });
 };
+
+// import { authRoutes } from "./auth.js";
+// import { orderRoutes } from "./order.js";
+// import { categoryRoutes, productRoutes } from "./products.js";
+
+// export const registerRoutes = async (fastify) => {
+//   fastify.register(authRoutes,      { prefix: "/api" });
+//   fastify.register(productRoutes,   { prefix: "/api" });
+//   fastify.register(categoryRoutes,  { prefix: "/api" });
+//   fastify.register(orderRoutes,     { prefix: "/api" });
+// };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,26 +1,26 @@
-// import { getAllCategories } from "../controllers/product/category.js";
-// import { getProductsByCategoryId } from "../controllers/product/product.js";
-
-// export const categoryRoutes = async (fastify, options) => {
-//   fastify.get("/categories", getAllCategories);
-// };
-
-// export const productRoutes = async (fastify, options) => {
-//   fastify.get("/products/:categoryId", getProductsByCategoryId);
-// };
-
 import { getAllCategories } from "../controllers/product/category.js";
-import {
-  getAllProducts,
-  getProductsByCategoryId,
-} from "../controllers/product/product.js";
+import { getProductsByCategoryId } from "../controllers/product/product.js";
 
-export const categoryRoutes = async (fastify) => {
+export const categoryRoutes = async (fastify, options) => {
   fastify.get("/categories", getAllCategories);
 };
 
-export const productRoutes = async (fastify) => {
-  fastify.get("/products", getAllProducts);
+export const productRoutes = async (fastify, options) => {
   fastify.get("/products/:categoryId", getProductsByCategoryId);
 };
+
+// import { getAllCategories } from "../controllers/product/category.js";
+// import {
+//   getAllProducts,
+//   getProductsByCategoryId,
+// } from "../controllers/product/product.js";
+
+// export const categoryRoutes = async (fastify) => {
+//   fastify.get("/categories", getAllCategories);
+// };
+
+// export const productRoutes = async (fastify) => {
+//   fastify.get("/products", getAllProducts);
+//   fastify.get("/products/:categoryId", getProductsByCategoryId);
+// };
 
